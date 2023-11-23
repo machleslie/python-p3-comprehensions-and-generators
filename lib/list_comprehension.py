ae92c4181a92b8e6c3708c5dddeb490166658cf6
machleslie <machariales@gmail.com>
2023-11-23 17:58:02 +0300

    Completed assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
-
 def return_evens(num_list):
-    pass
+    numbers = list(num_list)
+    evens = list()
+    for num in (numbers):
+        if num % 2 == 0:
+            evens.append(num)
+    return(evens)
+         
+        
+        
+    
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_sentences = []
+    for sentence in sentence_list:
+        if sentence != '':
+            exclamation_sentence = sentence + '!'
+            exclamation_sentences.append(exclamation_sentence)
+    return exclamation_sentences
+
+
+make_exclamation("")
